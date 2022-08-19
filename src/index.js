3fcdf6b758cf2709c1dfda26db68b76c7f8af3cc
s-berntsson <susanna.berntsson2000@gmail.com>
2022-08-19 21:47:44 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import TriangleNav from './routes/TriangleNav';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    
     <Provider store={store}>
-      <App />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <App /> }>
+            <Route index element={ <TriangleNav /> } />
+            <Route path="about" element={<h1>ABOUT</h1> } />
+            <Route path="art-gallery" element={<h1>ART GALLERY</h1> } />
+            <Route path="projects" element={<h1>PROJECTS</h1> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </Provider>
+
   </React.StrictMode>
 );
 
---
+++ src/routes/TriangleNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import triangle from "../assets/triangle.svg"
+
+const TriangleNav = () => {
+    return (
+        <nav>
+            <Link to="/about">ABOUT</Link>
+            <img src={triangle} alt="triangle" />
+            <Link to="/art-gallery">ART GALLERY</Link>
+            <Link to="/projects">PROJECTS</Link>
+        </nav>
+    )
+}
+
+export default TriangleNav

d3ff472551694865bc9642b8bf8f30a9e35c3330
s-berntsson <susanna.berntsson2000@gmail.com>
2022-08-20 16:50:15 +0200

    attempting to use svg as background

--- src/routes/index/TriangleNav.js
+++ src/routes/index/TriangleNav.js
@@ -1,14 +1,27 @@
 import React from "react";
 import {Link} from "react-router-dom"
 import triangle from "./triangle.svg"
+import styles from './TriangleNav.module.css'
 
 const TriangleNav = () => {
     return (
-        <nav>
-            <Link to="/about">ABOUT</Link>
-            <img src={triangle} alt="triangle" />
-            <Link to="/art-gallery">ART GALLERY</Link>
-            <Link to="/projects">PROJECTS</Link>
+        <nav className={styles.gridContainer}>
+            <Link className={styles.gridItem} id={styles.about} to="/about"><h4>ABOUT</h4></Link>
+            <section className={styles.gridItem} id={styles.triangle}>
+                <img className={styles.triangleSvg} src={triangle} alt="triangle" />
+                <h2 className={styles.name}>
+                    SUSANNA<br/>
+                    &nbsp;&nbsp;BERNT<br/>
+                    &nbsp;&nbsp;&nbsp;&nbsp;SSON
+                </h2>
+                <h1 className={styles.title}>
+                    POR<br/>
+                    TFO<br/>
+                    &nbsp;LIO
+                </h1>
+            </section>
+            <Link className={styles.gridItem} id={styles.artGallery} to="/art-gallery"><h4>ART GALLERY</h4></Link>
+            <Link className={styles.gridItem} id={styles.projects} to="/projects"><h4>PROJECTS</h4></Link>
         </nav>
     )
 }
